388f67ddf6d316754d9db335268e479e84ce3500
Eric Munyi <irerieric@gmail.com>
2023-04-22 19:11:39 -0700

    /: display “Hello HBNB!”

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# script that starts a Flask web application
+# must be listening on 0.0.0.0, port 5000
+# /: display “Hello HBNB!” & /hbnb: display “HBNB”
+
+from flask import Flask
+
+app = Flask("__name__")
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return ("Hello HBNB")
+
+@app.route("/hbnb", stict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
