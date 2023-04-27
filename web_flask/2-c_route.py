bb55ac7e5ad4e103ee9d1afa55da644c3dfff2f5
Eric Munyi <irerieric@gmail.com>
2023-04-26 19:14:54 -0700

    3 routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-# script that starts a Flask web application
-# must be listening on 0.0.0.0, port 5000
+"""Script that starts a Flask web application.
+The application must be listening on 0.0.0.0, port 5000.
+Routes:
+    - /: Displays 'Hello HBNB!'
+    - /hbnb: Displays 'HBNB'
+    - /c/<text>: Displays 'C ' followed by the value of the text variable,
+                 with underscores replaced with spaces
+"""
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-    return ("Hello HBNB!")
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return ("HBNB")
+    """Displays 'HBNB'"""
+    return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def cText(text):
+    """Displays 'C ' followed by the value of the text variable,
+    with underscores replaced with spaces"""
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
