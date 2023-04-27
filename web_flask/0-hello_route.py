e51fddb2cfc7cf46f77134ad54f2323ad5ad93fe
Eric Munyi <irerieric@gmail.com>
2023-04-26 19:03:42 -0700

    1 route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web app listening on 0.0.0.0, port 5000.
-"""
+""" This script starts a Flask web app listening on 0.0.0.0, port 5000 """
 
 from flask import Flask
 
