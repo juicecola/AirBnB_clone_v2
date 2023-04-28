b7ccb211c824aaaf19dca9e814b2a9b4ea565a4c
Eric Munyi <irerieric@gmail.com>
2023-04-27 19:11:39 -0700

    five routes

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,20 +9,24 @@ from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns a given string"""
     return ("HBNB")
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
     """Display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -30,11 +34,13 @@ def pythonText(text="is cool"):
     """Display Python followed by the value of the text variable"""
     return "Python {}".format(text.replace("_", " "))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def isNumber(n):
     """Display “n is a number” only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
